2c1472b6fbc83a56533c0753bc2d6bd460c58ecb
AudyDevs <eudaldclariana@gmail.com>
2024-05-07 16:47:06 +0200

    Added app colors and keyboard function extension

---
+++ app/src/main/java/com/example/timewise/core/Keyboard.kt
@@ -0,0 +1,16 @@
+package com.example.timewise.core
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Context.showKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.showSoftInput(view, 0)
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
